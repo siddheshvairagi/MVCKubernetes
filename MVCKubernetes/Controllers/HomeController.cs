dd3110afbd6a583554d6482c091e5a035a5ad24a
siddheshvairagi <31593196+siddheshvairagi@users.noreply.github.com>
2025-06-22 18:49:38 +0530

    commit

--- MVCKubernetes/Controllers/HomeController.cs
+++ MVCKubernetes/Controllers/HomeController.cs
@@ -21,6 +21,7 @@ namespace MVCKubernetes.Controllers
         public IActionResult Privacy()
         {
             return View();
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
